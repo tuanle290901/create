f9d21c54fd69b667a7ecb1b258a5ab941cc9e8a4
tuanle290901 <tuanle290901@gmail.com>
2023-03-08 16:04:26 +0700

    doitiente

--- day6-07.03/SDtoantu.js
+++ day6-07.03/SDtoantu.js
@@ -2,10 +2,9 @@ function convertCurrency() {
     let amount= document.getElementById('sotien').value
     let from= document.getElementById('From').value
     let to= document.getElementById('To').value
-  // if (from == "USD" && to == "VND")
-
+  if (from == "USD" && to == "VND")
     document.getElementById("result").innerHTML = Number(amount) * 23000
-    // console.log(Number(amount) *23000)
-    // alert(Number(amount) *23000)
+    if ( from == "VND" && to == "USD")
+    document.getElementById("result").innerHTML = Number(amount) / 23000
 
 }
--- day6-07.03/tinhtoan.js
+++ day6-07.03/tinhtoan.js
@@ -1,10 +1,37 @@
-function result () {
+function cong() {
     let so1=+document.getElementById('firstnumber').value
     let so2=+document.getElementById('andnumber').value
-    let congg= so1 + so2
-    let tru= so1 - so2
+    let congg = so1 + so2
 
-    document.getElementById('result').innerText = congg
+
+    document.getElementById('result').innerHTML = congg
+
+
+}
+function tru() {
+
+    let so1=+document.getElementById('firstnumber').value
+    let so2=+document.getElementById('andnumber').value
+    let tru = so1 - so2
+
+
     document.getElementById('result').innerHTML = tru
+}
+function nhan() {
 
+    let so1=+document.getElementById('firstnumber').value
+    let so2=+document.getElementById('andnumber').value
+    let nhan = so1 * so2
+
+
+    document.getElementById('result').innerHTML = nhan
+}
+function chia() {
+
+    let so1=+document.getElementById('firstnumber').value
+    let so2=+document.getElementById('andnumber').value
+    let chia = so1 / so2
+
+
+    document.getElementById('result').innerHTML = chia
 }
