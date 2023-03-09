5256b1d0e22d7bed8d7ddd2681b3fd131c39a958
tuanle290901 <tuanle290901@gmail.com>
2023-03-09 15:29:48 +0700

    tính chỉ số BMI

---
+++ day8-09.03/ktra_BMI.js
@@ -0,0 +1,20 @@
+function kiemtra() {
+    let cs1 = document.getElementById("cannang").value
+    let cs2 = document.getElementById("chieucao").value
+    let kq = cs1 / (cs2 ^ 2)
+
+    if (kq < 18.5) {
+        document.getElementById('ketqua').innerHTML = 'Chỉ số BMI của bạn là ' + kq + ' Bạn thiếu cân, đớp nhiều vào nhé' +
+            '<img src="3b97635b6bd4a5bfc436fbd3908f05e0.gif" height="50" width="100"/>'
+    } else if (kq >= 18.5 && kq < 25.0) {
+        document.getElementById('ketqua').innerHTML = 'Chỉ số BMI của bạn là ' + kq + ', Bạn bình thường'
+    } else if (kq >= 25 && kq < 30.0) {
+        document.getElementById('ketqua').innerHTML = 'Chỉ số BMI của bạn là ' + kq + ', Bạn bị béo phì rồi, Đớp ít thôi' +
+            '<img src="734d11cc677595ce3c3e5e3bba02721f.gif" height="93" width="45"/>'
+    } else {
+        document.getElementById('ketqua').innerHTML = 'Chỉ số BMI của bạn là ' + kq + ', Bạn bị béo phì rồi, Đớp ít thôi' +
+            '<img src="734d11cc677595ce3c3e5e3bba02721f.gif" height="93" width="45"/>'
+    }
+}
+
+}
